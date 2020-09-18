115b84d60e5510c3f182012164dbe68ab888ca0f
Marcin Wątroba <marcin.watroba@deviniti.com>
2020-09-18 07:32:00 +0200

    Change tests

--- src/integrationTest/kotlin/pl/theliver/tweetidscrapper/presentation/rest/ScrapTweetControllerTest.kt
+++ src/integrationTest/kotlin/pl/theliver/tweetidscrapper/presentation/rest/ScrapTweetControllerTest.kt
@@ -3,19 +3,16 @@ package pl.theliver.tweetidscrapper.presentation.rest
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.provider.Arguments
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.MvcResult
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-
+import pl.theliver.tweetidscrapper.application.ScrapGateway
+import pl.theliver.tweetidscrapper.domain.TweetId
+import pl.theliver.tweetidscrapper.presentation.rest.dto.TweetResultDto
 
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension::class)
@@ -23,35 +20,39 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 internal class ScrapTweetControllerTest {
 
     @Autowired
-    lateinit var mockMvc: MockMvc
+    lateinit var scrapGateway: ScrapGateway
 
     @Test
-    fun testGetBirthdayDOW() {
-        readTestData().stream().forEach { testScrapTweet(it) }
+    fun testScrapTweets() {
+        readTestData().forEach { testScrapTweet(it) }
     }
 
-    private fun testScrapTweet(testCase: TextCase) {
-        val path = "/scrapTweet/${testCase.id}"
-        val result: MvcResult = mockMvc.perform(MockMvcRequestBuilders.get(path)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk)
-                .andReturn()
-
-        val resultContent = result.response.contentAsByteArray.toString(Charsets.UTF_8)
-        assertNotNull(resultContent)
-        assertEquals(testCase.response, resultContent)
+    fun testScrapTweet(textCase: TextCase) {
+        val response = scrapGateway.scrapTweetBy(TweetId(textCase.id))
+                .let { TweetResultDto.from(it) }
+        val responseJson = Gson().toJson(response)
+        assertEquals(textCase.response, responseJson)
     }
 
-    private fun readTestData(): List<TextCase> {
-        val classLoader = ScrapTweetControllerTest::class.java.classLoader!!
-        val stream = classLoader.getResourceAsStream("test_data.json")!!
-        val content = stream.readAllBytes().toString(Charsets.UTF_8)
-        return Gson().fromJson(content)
+
+    data class TextCase(val id: String, val response: String, val code: Int) : Arguments {
+        override fun get(): Array<Any> {
+            return listOf(id, response, code).toTypedArray()
+        }
     }
 
-    private inline fun <reified T> Gson.fromJson(json: String) = fromJson<T>(json, object : TypeToken<T>() {}.type)
+    companion object {
+        fun readTestData(): Array<TextCase> {
+            val classLoader = ScrapTweetControllerTest::class.java.classLoader!!
+            val stream = classLoader.getResourceAsStream("test_data.json")!!
+            val content = stream.readAllBytes().toString(Charsets.UTF_8)
+            val list: List<TextCase> = Gson().fromJson(content)
+//            return list
+            return list.toTypedArray()
+        }
 
+        private inline fun <reified T> Gson.fromJson(json: String) = fromJson<T>(json, object : TypeToken<T>() {}.type)
 
-    data class TextCase(val id: String, val response: String, val code: Int)
+    }
 
 }
